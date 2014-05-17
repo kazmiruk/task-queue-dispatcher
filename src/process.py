488e4fee1d1c1c3e840c8a479ad2ab353d7f53aa
kazmiruk <kazmiruk.evgeniy@gmail.com>
2014-05-17 15:47:55 +0800

    #659: sentry logging

--- src/process.py
+++ src/process.py
@@ -1,16 +1,9 @@
 from gevent import monkey; monkey.patch_all()
-from interfaces.options import OPTIONS
-from importlib import import_module
-from interfaces.jobs import JobWrapper
 import logging
+from logging.config import dictConfig
 import settings
 
-# configure loggin lib from settings and options
-logging.basicConfig(
-    format=settings.LOGGING['format'],
-    level=settings.LOGGING['level'],
-    filename=OPTIONS.logfile
-)
+dictConfig(settings.LOGGING)
 
 
 def start_demon(obj):
@@ -21,17 +14,25 @@ def start_demon(obj):
     from interfaces.db import Connect
     Connect().start(obj())
 
-module = import_module("processors.{}".format(OPTIONS.processor.lower()))
-
-if not hasattr(module, OPTIONS.processor):
-    raise KeyError("Can't find pointed processor")
-
-processor_cls = getattr(module, OPTIONS.processor)
 
 try:
+    from interfaces.options import OPTIONS
+    from importlib import import_module
+    from interfaces.jobs import JobWrapper
+
+    module = import_module("processors.{}".format(OPTIONS.processor.lower()))
+
+    if not hasattr(module, OPTIONS.processor):
+        raise KeyError("Can't find pointed processor")
+
+    processor_cls = getattr(module, OPTIONS.processor)
+
     start_demon(processor_cls)
     #all spawned jobs before this string are joined in one waiter
     JobWrapper.join_all()
 except KeyboardInterrupt, e:
     logging.debug("Demon shutdown")
     JobWrapper.kill_all()
+except Exception, e:
+    logging.error(e)
+    raise e
--- src/settings/default.py
+++ src/settings/default.py
@@ -24,9 +24,32 @@ WAIT_DB_RESPONSE = 10
 
 DEBUG = False
 
-import logging
-
 LOGGING = {
-    'format': u'%(asctime)-15s: %(levelname)s: %(filename)s:%(lineno)d: %(message)s',
-    'level': logging.DEBUG
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'console': {
+            'format': u'%(asctime)-15s: %(levelname)s: %(filename)s:%(lineno)d: %(message)s',
+            'datefmt': '%d-%m-%Y %H:%M:%S',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        },
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.handlers.logging.SentryHandler',
+            'dsn': 'udp://c01073c86dd84fe28eccc363374245d6:d0630bf10200461f92bf75f7995a89a2@sentry.dev.pearbox.net:9001/6'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'sentry'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
+    }
 }
